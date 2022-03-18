dadf10c1f3898abc4f30db17ea51ea4af31202a8
Ibrohim <irgashev1261@gmail.com>
2022-03-18 10:45:53 +0500

    add index with html, create a class with complete and isComplete methods

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,6 +1,33 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once 'functions.php';
 
+class Task
+{
+    public string $description;
+    public bool $completed = false;
 
-dd('sad');
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete(): bool
+    {
+        return $this->completed;
+    }
+}
+
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Clean house'),
+    new Task('Work hard'),
+];
+
+$tasks['2']->complete();
+
+require_once 'index.view.php';
---
+++ CRUD/index.view.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<ul>
+    <?php foreach ($tasks as $task) : ?>
+        <li>
+            <?php if ($task->completed) : ?>
+                <strike><?= $task->description ?> </strike>
+            <?php else : ?>
+                <?= $task->description ?>
+            <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+</body>
+</html>
