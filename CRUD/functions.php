5cc17a915ba3993e5cd4a376b5eded719c4038c8
Ibrohim <irgashev1261@gmail.com>
2022-03-18 11:14:38 +0500

    add connection method and fetchAllTasks

--- CRUD/functions.php
+++ CRUD/functions.php
@@ -1,8 +1,33 @@
 <?php
 
+function connectToDb()
+{
+    try {
+        return new PDO('mysql:host=localhost;dbname=academy', 'ibra', 'Password123$');
+    } catch (PDOException $exception) {
+        die($exception->getMessage());
+    }
+}
+
+function fetchAllTasks($pdo)
+{
+    $statement = $pdo->prepare('SELECT * FROM todos');
+
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
+
 function dd($data)
 {
     echo "<pre>";
     die(var_dump($data));
     echo "<pre>";
+}
+
+function myErrors()
+{
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 }
--- CRUD/index.php
+++ CRUD/index.php
@@ -1,33 +1,11 @@
 <?php
+
+
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once 'functions.php';
+require_once 'Task.php';
+$pdo = ConnectToDb();
 
-class Task
-{
-    public string $description;
-    public bool $completed = false;
-
-    public function __construct($description)
-    {
-        $this->description = $description;
-    }
-
-    public function complete()
-    {
-        $this->completed = true;
-    }
-
-    public function isComplete(): bool
-    {
-        return $this->completed;
-    }
-}
-
-$tasks = [
-    new Task('Go to the store'),
-    new Task('Clean house'),
-    new Task('Work hard'),
-];
-
-$tasks['2']->complete();
+$tasks = FetchAllTasks($pdo);
 
 require_once 'index.view.php';
