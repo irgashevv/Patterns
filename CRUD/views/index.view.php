38f0d2ba65fa0589fa703770e82c8ab0cba268ac
Ibrohim <irgashev1261@gmail.com>
2022-03-18 15:27:33 +0500

    add header and footer:

--- CRUD/views/index.view.php
+++ CRUD/views/index.view.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
+<?php require 'header.php'; ?>
+<h1>My tasks</h1>
 <ul>
     <?php foreach ($tasks as $task) : ?>
         <li>
@@ -20,6 +11,4 @@
         </li>
     <?php endforeach; ?>
 </ul>
-
-</body>
-</html>
+<?php require 'footer.php'; ?>
